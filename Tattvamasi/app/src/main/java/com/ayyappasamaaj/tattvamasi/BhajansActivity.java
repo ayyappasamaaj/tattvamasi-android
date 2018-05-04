458bafd54b33c3bcf00a4ccbf1ffad3a4102ef76
Sathis Kumar <sathis.star@gmail.com>
2018-05-03 21:22:36 -0700

    Home screen and other connecting pages added

---
+++ Tattvamasi/app/src/main/java/com/ayyappasamaaj/tattvamasi/BhajansActivity.java
@@ -0,0 +1,12 @@
+package com.ayyappasamaaj.tattvamasi;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class BhajansActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bhajans);
+    }
+}
--- Tattvamasi/app/src/main/java/com/ayyappasamaaj/tattvamasi/HomeActivity.java
+++ Tattvamasi/app/src/main/java/com/ayyappasamaaj/tattvamasi/HomeActivity.java
@@ -1,7 +1,9 @@
 package com.ayyappasamaaj.tattvamasi;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +11,35 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void OnBhajanClick(View view){
+        Intent intent = new Intent(this, BhajansActivity.class);
+        this.startActivity(intent);
+    }
+
+    public void OnPoojaClick(View view){
+        Intent intent = new Intent(this, PoojaActivity.class);
+        this.startActivity(intent);
+    }
+
+    public void OnArticlesClick(View view){
+        Intent intent = new Intent(this, ArticlesActivity.class);
+        this.startActivity(intent);
+    }
+
+    public void OnEventsClick(View view){
+        Intent intent = new Intent(this, EventsActivity.class);
+        this.startActivity(intent);
+    }
+
+    public void OnAboutUsClick(View view){
+        Intent intent = new Intent(this, AboutUsActivity.class);
+        this.startActivity(intent);
+    }
+
+    public void OnDonateClick(View view){
+        Intent intent = new Intent(this, DonateActivity.class);
+        this.startActivity(intent);
     }
 }
