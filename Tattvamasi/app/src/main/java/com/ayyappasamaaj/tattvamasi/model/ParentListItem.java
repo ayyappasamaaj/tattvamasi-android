7334a2f0b0ab8c2b473bf02ebee7a2ddf726a787
Satya Surya <satyanarayan.suryanarayanan1@usbank.com>
2018-10-21 22:00:24 -0700

    Image change

--- Tattvamasi/app/src/main/java/com/ayyappasamaaj/tattvamasi/model/ParentListItem.java
+++ Tattvamasi/app/src/main/java/com/ayyappasamaaj/tattvamasi/model/ParentListItem.java
@@ -5,7 +5,9 @@ public class ParentListItem {
 
     public ParentListItem() {}
 
-    public ParentListItem(String name) {this.name = name;}
+    public ParentListItem(String name) {
+        this.name = toTitleCase(name);
+    }
 
     public String getName() {
         return name;
@@ -13,5 +15,16 @@ public class ParentListItem {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    private static String toTitleCase(String givenString) {
+        String[] arr = givenString.split(" ");
+        StringBuffer sb = new StringBuffer();
+
+        for (int i = 0; i < arr.length; i++) {
+            sb.append(Character.toUpperCase(arr[i].charAt(0)))
+                    .append(arr[i].substring(1)).append(" ");
+        }
+        return sb.toString().trim();
     }
 }
